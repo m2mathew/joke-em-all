e005bc258504e948cd942af5d6af490afc75ca31
Mike Mathew <m2mathew@me.com>
2019-09-24 15:46:34 -0500

    Add shared 'Separator' component

---
+++ components/Separator.js
@@ -0,0 +1,21 @@
+// External Dependencies
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+// Component Definition
+function Separator() {
+  return <View style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
+export default Separator;
